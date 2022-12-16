41c8ad89eb31d510f765bc4c828c6b702bd973a9
Kaden C <115026029+Khoieee@users.noreply.github.com>
2022-12-16 02:00:12 -0800

    comments

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'Kaden Can, Period 3, Intro To Programming'
+'https://www.youtube.com/watch?v=VUFvY349ess'
+'https://www.youtube.com/watch?v=rWtfClpWSb8&t=0s - Delta Time'
+'https://www.youtube.com/watch?v=uW3Fhe-Vkx4&t=0s - Masks'
+
+
+
 import pygame, sys, time
 from settings import *
 from sprites import *
@@ -7,9 +14,12 @@ class Game:
         
         # setup
         pygame.init()
+        #creates a display surface for the game
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        # names the display Flappy Bird
         pygame.display.set_caption('Flappy Bird')
         self.clock = pygame.time.Clock()
+        # track the status of the game (see if running)
         self.active = True
 
         # sprite groups
@@ -17,52 +27,72 @@ class Game:
         self.collision_sprites = pygame.sprite.Group()
 
         # scale factor
+        # gets the height of the window and divides it by the height of the image to find the scale factor needed to stretch the image
         bg_height = pygame.image.load('background.png').get_height()
         self.scale_factor = WINDOW_HEIGHT / bg_height
 
         # sprite setup
         BG(self.all_sprites,self.scale_factor)
+        # ground is part of the collision sprites and kills plane if touched
         Ground([self.all_sprites,self.collision_sprites],self.scale_factor)
+        # divide scale factor by two to make the plane smaller
         self.plane = Plane(self.all_sprites,self.scale_factor/ 2)
 
         # timer
         self.obstacle_timer = pygame.USEREVENT + 1
+        # starts the timer 1400ms is how many times it should run
         pygame.time.set_timer(self.obstacle_timer,1400)
 
         # text
+        # uses the font BD Cartoon Shout size 30
+        # sets the starting score to 0
+        # sets the offset score to 0 when restarting game
         self.font = pygame.font.Font('BD_Cartoon_Shout.ttf',30)
         self.score = 0
         self.start_offset = 0
 
         # menu
+        # loads the menu image
+        # sets the menu image in the middle of the display surface
         self.menu_surf = pygame.image.load('menu.png').convert_alpha()
         self.menu_rect = self.menu_surf.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
+    # method for collisions
     def collisions(self):
+        # check if there is any collision between plan and obstacles
          if pygame.sprite.spritecollide(self.plane,self.collision_sprites,False,pygame.sprite.collide_mask)\
-        or self.plane.rect.top <= 0:
+        or self.plane.rect.top <= 0: #makes sure the plane cant go throught the top of the display
+        # checking if obstacle that collides with plane is part of sprite_type obstacle
             for sprite in self.collision_sprites.sprites():
                 if sprite.sprite_type == 'obstacle':
                     sprite.kill()
+            # when plane collides with obstacle turns off the status of the game class
             self.active = False
+            # when plane collides with obstacle kill plane
             self.plane.kill()
- 
+    # method for displaying score 
     def display_score(self):
         if self.active:
             self.score = (pygame.time.get_ticks() - self.start_offset) // 1000
             y = WINDOW_HEIGHT / 10
         else:
+            # once you die put the score under neath the menu
             y = WINDOW_HEIGHT / 2 + (self.menu_rect.height / 1.5)
 
+        # creating surface of score
         score_surf = self.font.render(str(self.score),True,'black')
+        # puts the score in the topcenter
         score_rect = score_surf.get_rect(midtop = (WINDOW_WIDTH / 2,y))
+        # blitting onto the screen
         self.display_surface.blit(score_surf,score_rect)
 
+    # run method
     def run(self):
         last_time = time.time()
+        # updating pygame
         while True:
             
-            # delta time
+            # delta time - there to account for different framerates
             dt = time.time() - last_time
             last_time = time.time()
  
@@ -71,10 +101,13 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                    # when mouse button is pressed the plane 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.active:
+                        # plane jumps
                         self.plane.jump()
                     else:
+                        # looping status of game and clock
                         self.plane = Plane(self.all_sprites,self.scale_factor/ 2)
                         self.active = True
                         self.start_offset = pygame.time.get_ticks()
@@ -83,19 +116,26 @@ class Game:
                     Obstacle([self.all_sprites,self.collision_sprites],self.scale_factor/1.7)
             
             # game logic
+            # fill the space around background as black
             self.display_surface.fill('black')
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.display_surface)
             self.display_score()
 
+            # while game is active set collisions true
             if self.active:
                 self.collisions()
             else:
+                # if game is over start menu screen
                 self.display_surface.blit(self.menu_surf,self.menu_rect)
 
             pygame.display.update()
+            # updates the framerate
             self.clock.tick(FRAMERATE)
- 
+
+ # checking if current file is main file
 if __name__ == '__main__':
+    # creating one instance of the game class
     game = Game()
+    # calling game.run()
     game.run()
--- sprites.py
+++ sprites.py
@@ -5,115 +5,142 @@ from random import choice, randint
 class BG(pygame.sprite.Sprite):
     def __init__(self,groups,scale_factor):
         super().__init__(groups)
+        # loading image for background
         bg_image = pygame.image.load('background.png').convert()
 
+        # scaling the height and the width to neatly fit the display surface
         full_height = bg_image.get_height() * scale_factor
         full_width = bg_image.get_width() * scale_factor
         full_sized_image = pygame.transform.scale(bg_image,(full_width,full_height))
 
+        # making the image double the width to help with scrolling
         self.image = pygame.Surface((full_width * 2,full_height))
+        # centers the background
         self.image.blit(full_sized_image,(0,0))
         self.image.blit(full_sized_image,(full_width,0))
 
+        # sets the coords of the background making it cover the whole display surface.
         self.rect = self.image.get_rect(topleft = (0,0))
         self.pos = pygame.math.Vector2(self.rect.topleft)
-
+    # makes the background scroll past at a certain speed. If the center is less that zero it will repeat
     def update(self,dt):
         self.pos.x -= 300 * dt
         if self.rect.centerx <= 0:
             self.pos.x = 0
         self.rect.x = round(self.pos.x)
 
+# makes ground the sprite type ground
+# setting up ground class
 class Ground(pygame.sprite.Sprite):
     def __init__(self,groups,scale_factor):
         super().__init__(groups)
         self.sprite_type = 'ground'
 
         # image
+        # loads image for ground sprite
+        # convert alpha to get rid of transparent background
         ground_surf = pygame.image.load('ground.png').convert_alpha()
         self.image = pygame.transform.scale(ground_surf,pygame.math.Vector2(ground_surf.get_size()) * scale_factor)
 
         #position
+        # sets the position of the ground at the bottom
         self.rect = self.image.get_rect(bottomleft = (0,WINDOW_HEIGHT))
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
         # mask
+        # masking the ground image so you can actually go into the crevices
         self.mask = pygame.mask.from_surface(self.image)
-
+    # method for updating the ground
+    # makes the ground scroll past at a certain speed
+    # makes it loop if it reaches it
     def update(self,dt):
         self.pos.x -= 360 * dt
         if self.rect.centerx <= 0:
             self.pos.x =0
 
         self.rect.x = round(self.pos.x)
-
+# plane class 
 class Plane(pygame.sprite.Sprite):
     def __init__(self,groups,scale_factor):
         super().__init__(groups)
 
         # image
+        # helps setup the image animation
         self.import_frames(scale_factor)
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
 
         # rect
+        # sets the position of the plane on the displane surface
         self.rect = self.image.get_rect(midleft = (WINDOW_WIDTH / 20, WINDOW_HEIGHT / 2))
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
         # movement
+        # sets the gravity of the plane and the direction pulling it
         self.gravity = 600
         self.direction = 0
 
         # mask
+        # masking the plane so it doesn't clip on the transparent edges
         self.mask = pygame.mask.from_surface(self.image)
 
     def import_frames(self, scale_factor):
         self.frames = []
         for i in range(3):
+            # loads the three images for the plane so it seems as if the propelors are spinning
             surf = pygame.image.load(f'red{i}.png').convert_alpha()
             scaled_surface = pygame.transform.scale(surf,pygame.math.Vector2(surf.get_size())* scale_factor)
             self.frames.append(scaled_surface)
 
+    #applying gravity to the plane
     def apply_gravity(self,dt):
         self.direction += self.gravity * dt
         self.pos.y += self.direction * dt
         self.rect.y = round(self.pos.y)
 
+    # makes the plane jump
     def jump(self):
         self.direction = - 400
 
+    # makes the images rotate in specific order to make it animated
     def animate(self,dt):
         self.frame_index += 10 * dt
         if self.frame_index >= len(self.frames):
             self.frame_index = 0
         self.image = self.frames[int(self.frame_index)]
 
+    # rotates the plane as it goes in a downward direction
     def rotate(self,dt):
         rotated_plane = pygame.transform.rotozoom(self.image,-self.direction * 0.05,1)
         self.image = rotated_plane
         self.mask = pygame.mask.from_surface(self.image)
 
-
+    # updates that constantly apply gravity, animations, and rotations
     def update(self,dt):
         self.apply_gravity(dt)
         self.animate(dt)
         self.rotate(dt)
-
+# obstacle class
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self,groups,scale_factor):
         super().__init__(groups)
+        # setting sprite type for colllisions
         self.sprite_type = 'obstacle'
 
+        # sets the spikes either at the top or the bottom
         orientation = choice(('up','down'))
+        # chooses between variant of spike and removes transparent background of image
         surf = pygame.image.load(f'{choice((0,1))}.png').convert_alpha()
+        # sets the size of the images
         self.image = pygame.transform.scale(surf,pygame.math.Vector2(surf.get_size())* scale_factor)
         
         x = WINDOW_WIDTH + randint(40,100)
-
+        # oreination if on the top side
         if orientation == 'up':
             y = WINDOW_HEIGHT + randint(10,50)
             self.rect = self.image.get_rect(midbottom = (x,y))
         else:
+            # orientation if on the bottom side
             y = randint(-50,-10)
             self.image = pygame.transform.flip(self.image,False,True)
             self.rect = self.image.get_rect(midtop = (x,y))
@@ -121,9 +148,11 @@ class Obstacle(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
         # mask
+        # masking the spikes so plane doesn't clip the transparent edges
         self.mask = pygame.mask.from_surface(self.image)
 
-
+    # updates the obstacles so they can move acorss the screen
+    # once they are on the left side of the screen they are killed
     def update(self,dt):
         self.pos.x -= 400 * dt
         self.rect.x = round(self.pos.x)
